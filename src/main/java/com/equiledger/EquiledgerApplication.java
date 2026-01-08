24166daa35b009be00d97edd57034417ada1860b
Niranjana <sriniranjana1503@gmail.com>
2026-01-08 21:53:47 +0530

    Initial Commit

---
+++ src/main/java/com/equiledger/EquiledgerApplication.java
@@ -0,0 +1,13 @@
+package com.equiledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiledgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equiledger/EquiledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equiledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquiledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
